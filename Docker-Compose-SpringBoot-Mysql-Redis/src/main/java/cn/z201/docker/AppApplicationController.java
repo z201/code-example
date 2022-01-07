d303d7e9fcb0dadac5e3d609bb6f76e90908a2d8
z201 <z201.coding@gmail.com>
2022-01-07 22:46:04 +0800

    更新docker-compose

--- Docker-Compose-SpringBoot-Mysql-Redis/src/main/java/cn/z201/docker/AppApplicationController.java
+++ Docker-Compose-SpringBoot-Mysql-Redis/src/main/java/cn/z201/docker/AppApplicationController.java
@@ -1,10 +1,13 @@
 package cn.z201.docker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author z201.coding@gmail.com
@@ -13,12 +16,21 @@ import java.util.Map;
 @RestController
 public class AppApplicationController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @RequestMapping(value = "")
-    public Object index(){
-        Map<String,String> data = new HashMap<>();
-        data.put("code","200");
-        data.put("data","ok");
+    public Object index() {
+        List<String> dataBasesList = jdbcTemplate.queryForList("SHOW  DATABASES", String.class);
+        Properties info =  redisTemplate.getConnectionFactory().getConnection().info();
+        Map<String, Object> data = new HashMap<>();
+        data.put("code", "200");
+        data.put("db", dataBasesList.toString());
+        data.put("cache", info);
+        System.out.println(info.get("redis_version"));
         return data;
     }
-
 }
