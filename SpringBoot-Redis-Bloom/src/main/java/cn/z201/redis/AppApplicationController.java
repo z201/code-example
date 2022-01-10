18b06a562d00e470443264df2dda43c28513e858
z201 <z201.coding@gmail.com>
2022-01-11 01:46:33 +0800

    代码进度更新

---
+++ SpringBoot-Redis-Bloom/src/main/java/cn/z201/redis/AppApplicationController.java
@@ -0,0 +1,26 @@
+package cn.z201.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author z201.coding@gmail.com
+ **/
+@RestController
+@Slf4j
+public class AppApplicationController {
+
+    @RequestMapping(value = "{id}")
+    public Object index(@PathVariable(required = false) String id) {
+        log.info("id",id);
+        Map<String, Object> data = new HashMap<>();
+        data.put("code", "200");
+        data.put("data", id);
+        return data;
+    }
+}
