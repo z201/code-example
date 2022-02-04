fbe81ff74e0876d578db86c49775a253c6cfd237
z201 <z201.coding@gmail.com>
2022-02-04 13:39:08 +0800

    完善docker-compose 测试

--- Mybatis-Explain/src/main/java/cn/z201/mybatis/AppApplicationController.java
+++ Mybatis-Explain/src/main/java/cn/z201/mybatis/AppApplicationController.java
@@ -1,9 +1,13 @@
 package cn.z201.mybatis;
 
+import cn.z201.mybatis.dao.AccountDao;
+import cn.z201.mybatis.entity.Account;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,11 +16,25 @@ import java.util.Map;
 @RestController
 public class AppApplicationController {
 
+    @Resource
+    AccountDao accountDao;
+
     @RequestMapping(value = "")
     public Object index() {
+        Account account =  TestTool.create();
+        accountDao.insert(account);
         Map<String, Object> data = new HashMap<>();
         data.put("code", "200");
-        data.put("data", "ok");
+        data.put("data", account);
+        return data;
+    }
+
+    @RequestMapping(value = "list")
+    public Object list() {
+        List<Account> accountList = accountDao.selectList(null);
+        Map<String, Object> data = new HashMap<>();
+        data.put("code", "200");
+        data.put("data", accountList);
         return data;
     }
 }
--- Mybatis-Explain/src/test/java/cn/z201/mybatis/AppApplicationTest.java
+++ Mybatis-Explain/src/test/java/cn/z201/mybatis/AppApplicationTest.java
@@ -3,12 +3,6 @@ package cn.z201.mybatis;
 import cn.z201.mybatis.dao.AccountDao;
 import cn.z201.mybatis.entity.Account;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.github.stuxuhai.jpinyin.PinyinException;
-import com.github.stuxuhai.jpinyin.PinyinFormat;
-import com.github.stuxuhai.jpinyin.PinyinHelper;
-import io.codearte.jfairy.Fairy;
-import io.codearte.jfairy.producer.person.Person;
-import io.codearte.jfairy.producer.person.PersonProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer;
@@ -20,8 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.annotation.Resource;
-import java.time.Clock;
-import java.util.Locale;
 
 @Slf4j
 @ExtendWith(SpringExtension.class)
@@ -38,35 +30,11 @@ public class AppApplicationTest {
     @Disabled
     public void setup(){
         for (int i = 0; i < 1000; i++) {
-            accountDao.insert(create());
+            accountDao.insert(TestTool.create());
         }
     }
 
-    private Account create(){
-        Fairy fairy = Fairy.create(Locale.CHINA);
-        //设置生成年龄在21到27岁
-        Person person = fairy.person(
-                PersonProperties.ageBetween(18, 29),
-                PersonProperties.telephoneFormat("13#########"));//设置手机号码格式
-        String fullName = person.getFullName().replace(" ", "");
-        String email = "";
-        try {
-            email = PinyinHelper.convertToPinyinString(person.getEmail(), "", PinyinFormat.WITHOUT_TONE);
-            //EMail
-        } catch (PinyinException e) {
-            e.printStackTrace();
-        }
-        Long time = Clock.systemDefaultZone().millis();
-        Account account = new Account();
-        account.setIsEnable(true);
-        account.setCreateTime(time);
-        account.setUpdateTime(time);
-        account.setUsrName(fullName);
-        account.setEmail(email);
-        account.setPhoneNumber(person.getTelephoneNumber());
-        account.setSaltPassword(person.getPassword());
-        return account;
-    }
+
 
     @Test
     @Disabled
